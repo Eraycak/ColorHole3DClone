fedf92fa4610b0609327a835d75b14199cbe244c
Eray <77186648+Eraycak@users.noreply.github.com>
2022-06-22 19:44:16 +0300

    MoveBetweenPoints
    changes to second area after eating all blocks in first area

--- ColorHole3DClone/Assets/Scripts/KillControl.cs
+++ ColorHole3DClone/Assets/Scripts/KillControl.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class KillControl : MonoBehaviour
 {
-
+    private GameObject holeParent;
     private int eatableCount = 0;
     private int eatableCount2 = 0;
+    private bool changeIsTrue = false;
+    private bool movedFirstLocationIsTrue = false;
+    private Vector3 firstAreaPosition = new Vector3(0, 0, 0);
+    private Vector3 secondAreaPosition = new Vector3(0, 0, 65);
 
     private void Start()
     {
         eatableCount = GameObject.FindGameObjectsWithTag("EatableObjects").Length;
         eatableCount2 = GameObject.FindGameObjectsWithTag("EatableObjects2").Length;
+        holeParent = GameObject.Find("HoleParent");
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,6 +36,10 @@ public class KillControl : MonoBehaviour
             {
                 eatableCount2--;
             }
+            if (eatableCount == 0)
+            {
+                changeIsTrue = true;
+            }
             if (eatableCount2 == 0)
             {
                 Debug.LogError("nextlevel");
@@ -44,4 +53,24 @@ public class KillControl : MonoBehaviour
             Debug.LogError("vibrated");
         }
     }
+
+    private void Update()
+    {
+        if (changeIsTrue && holeParent.transform.position != firstAreaPosition)
+        {
+            holeParent.transform.position = Vector3.Lerp(holeParent.transform.position, firstAreaPosition, Time.deltaTime);
+        }
+
+        if (holeParent.transform.position == firstAreaPosition)
+        {
+            movedFirstLocationIsTrue = true;
+            changeIsTrue = false;
+        }
+
+        if (movedFirstLocationIsTrue && holeParent.transform.position != secondAreaPosition)
+        {
+            holeParent.transform.position = Vector3.Lerp(holeParent.transform.position, secondAreaPosition, Time.deltaTime);
+        }
+    }
+
 }
